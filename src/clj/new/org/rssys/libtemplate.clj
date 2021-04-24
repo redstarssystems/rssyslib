f775a9c313a62d57a01b0d0e008cc3e2ac61054a
Mike Ananev <mike1452@yandex.ru>
2021-04-24 18:05:52 +0300

    2.0.0 release

--- src/clj/new/org/rssys/libtemplate.clj
+++ src/clj/new/org/rssys/libtemplate.clj
@@ -1,7 +1,7 @@
-(ns clj.new.rssyslib
+(ns clj.new.org.rssys.libtemplate
   (:require [clj.new.templates :refer [renderer project-data ->files]]))
 
-(defn rssyslib
+(defn libtemplate
   "entry point to run template."
   [name]
   (let [render (renderer "rssyslib")
@@ -17,15 +17,14 @@
       ["src/{{nested-dirs}}/core.clj" (render "src/core.clj" data)]
       ["test/{{nested-dirs}}/core_test.clj" (render "test/core_test.clj" data)]
       [".editorconfig" (render ".editorconfig" data)]
-      [".env.public" (render ".env.public" data)]
-      [".env.private" (render ".env.private" data)]
-      [".envrc" (render ".envrc" data)]
+      ["project-config.edn" (render "project-config.edn" data)]
+      ["project-secrets.edn" (render "project-secrets.edn" data)]
       [".cljstyle" (render ".cljstyle" data)]
       ["CHANGELOG.adoc" (render "CHANGELOG.adoc" data)]
       ["LICENSE" (render "LICENSE" data)]
-      ["Justfile" (render "Justfile" data)]
+      ["bb.edn" (render "bb.edn" data)]
       ["tests.edn" (render "tests.edn" data)]
-      ["version_id" (render "version_id" data)]
+      ["project-version" (render "project-version" data)]
       ["README.adoc" (render "README.adoc" data)]
       ["scripts/bump-semver.clj" (render "scripts/bump-semver.clj" data)]
       ["pom.xml" (render "pom.xml" data)])))
